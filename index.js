9ea75056ce0527ac38f01dbafddef1cd798840ec
marco-gulino <marcogulino98@gmail.com>
2025-02-04 15:34:49 +0100

    [feat] added static assets

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+//Richiamo assets statici (images)
+app.use(express.static("public"));
+
+//Base route
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.type("html")
+    .send("<h1>Server del mio blog</h1>");
 });
 
 app.listen(port, () => {
