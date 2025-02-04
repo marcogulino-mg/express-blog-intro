536994c65974693a5e03f748ffb4f6e65441b05f
marco-gulino <marcogulino98@gmail.com>
2025-02-04 15:35:56 +0100

    [feat] added second route (bacheca)

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-//Richiamo assets statici (images)
+//Richiamo assets statici (public)
 app.use(express.static("public"));
 
 //Base route
 app.get("/", (req, res) => {
-  res.type("html")
-    .send("<h1>Server del mio blog</h1>");
+  res.type("html").send("<h1>Server del mio blog</h1>");
+});
+
+//Second route (bacheca)
+app.get("/bacheca", (req, res) => {
+  res.send("Sei dentro la route bacheca");
 });
 
 app.listen(port, () => {
